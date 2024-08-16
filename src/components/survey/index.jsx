8e835319bacb455ade94d7c5f801d431b3bb812e
Alexandre Mota <alexandre.acustica@gmail.com>
2024-08-16 17:01:14 -0300

    Add CSS and enhance anamnese model handling
    Introduce new CSS for the anamnese page to resolve layout issues. Additionally, enhance anamnese model handling to conditionally hide elements, improve SurveyCreatorWidget, and make backend API call for saving survey data. Also refactor menus for better navigation.

--- src/components/survey/index.jsx
+++ src/components/survey/index.jsx
@@ -1,41 +1,104 @@
 // Uncomment the following line if you are using Next.js:
 // 'use client'
 
+import React, {useEffect, useState} from "react";
 import {SurveyCreator, SurveyCreatorComponent} from "survey-creator-react";
 import "survey-core/defaultV2.min.css";
 import "survey-creator-core/survey-creator-core.min.css";
 import "survey-core/survey.i18n.js";
 import "survey-creator-core/i18n/portuguese";
+import api from "../../axios.js";
 
 import {editorLocalization} from "survey-creator-core";
 
 editorLocalization.currentLocale = "pt";
-// ----- or -----
-
-// Survey.surveyLocalization.defaultLocale = "pt";
-
 
 const creatorOptions = {
     showLogicTab: false,
     isAutoSave: true,
-    questionTypes: ["text", "checkbox", "comment", "radiogroup", "dropdown", "ranking", "rating"],
+    questionTypes: [
+        "text", "checkbox", "comment", "radiogroup", "dropdown",
+        "ranking", "rating"
+    ],
     showJSONEditorTab: false,
     // tabs: ["designer", "test"],
 };
 
 const defaultJson = {
+    title: "Nome do Grupo",
     pages: []
 };
 
+const clearOldLocalStorageEntries = (currentTitle) => {
+    for (let i = 0; i < window.localStorage.length; i++) {
+        const key = window.localStorage.key(i);
+        if (key.startsWith('modelo-anamnese-') && key !== `modelo-anamnese-${currentTitle}`) {
+            window.localStorage.removeItem(key);
+        }
+    }
+};
+
 export function SurveyCreatorWidget() {
-    const creator = new SurveyCreator(creatorOptions);
-    creator.text = window.localStorage.getItem("survey-json") || JSON.stringify(defaultJson);
-    creator.saveSurveyFunc = (saveNo, callback) => {
-        window.localStorage.setItem("survey-json", creator.text);
-        callback(saveNo, true);
-    };
+    const [surveyTitle, setSurveyTitle] = useState(defaultJson.title);
+    const [creator, setCreator] = useState(null);
+
+    useEffect(() => {
+        const storedJson = window.localStorage.getItem(`modelo-anamnese-${surveyTitle}`) || JSON.stringify(defaultJson);
+        const creatorInstance = new SurveyCreator(creatorOptions);
+        creatorInstance.text = storedJson;
+
+        creatorInstance.saveSurveyFunc = async (saveNo, callback) => {
+            const surveyData = JSON.parse(creatorInstance.text);
+            const newTitle = surveyData.title;
+
+            // Save to localStorage with new title
+            window.localStorage.setItem(`modelo-anamnese-${newTitle}`, creatorInstance.text);
+
+            // Remove old localStorage key if title has changed
+            if (newTitle !== surveyTitle) {
+                window.localStorage.removeItem(`modelo-anamnese-${surveyTitle}`);
+                setSurveyTitle(newTitle);
+            }
+
+            // Clear old local storage entries
+            clearOldLocalStorageEntries(newTitle);
+
+            // Send JSON to backend
+            try {
+                const response = await api.post('/teste/', {
+                    grupo: newTitle,
+                    surveyData: surveyData
+                });
+                console.log('Survey data saved successfully', response.data);
+                callback(saveNo, true); // Indicar sucesso para o Survey Creator
+            } catch (error) {
+                console.error('Error saving survey data', error);
+                callback(saveNo, false); // Indicar falha para o Survey Creator
+            }
+        };
+
+        if (creatorInstance.survey) {
+            // Atualiza o estado quando o survey é carregado ou editado
+            creatorInstance.survey.onPropertyChanged.add((sender, options) => {
+                if (options.name === 'title') {
+                    const newTitle = options.newValue;
+                    // Save automatically when the title changes
+                    window.localStorage.setItem(`modelo-anamnese-${newTitle}`, creatorInstance.text);
+                    window.localStorage.removeItem(`modelo-anamnese-${surveyTitle}`);
+                    setSurveyTitle(newTitle);
+                }
+            });
+        }
+
+        setCreator(creatorInstance);
+    }, [surveyTitle]);
+
     return (
-        <SurveyCreatorComponent creator={creator}/>
+        <div>
+
+            {creator && (
+                <SurveyCreatorComponent creator={creator}/>
+            )}
+        </div>
     );
 }
-
